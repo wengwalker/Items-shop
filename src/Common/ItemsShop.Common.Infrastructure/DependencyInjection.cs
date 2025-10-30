9be419f25eab632842b2ff91a941f791ab484157
wengwalker <wengwalkerhome@yandex.ru>
2025-10-30 17:41:58 +0300

    feat: added telemetry and tracing support

--- src/Common/ItemsShop.Common.Infrastructure/DependencyInjection.cs
+++ src/Common/ItemsShop.Common.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,36 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace ItemsShop.Common.Infrastructure;
 
 public static class DependencyInjectionExtensions
 {
-    public static IServiceCollection AddCoreInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddCoreInfrastructure(this IServiceCollection services, string[] activityModuleNames)
     {
+        services.AddHostOpenTelemetry(activityModuleNames);
+
+        return services;
+    }
+
+    private static IServiceCollection AddHostOpenTelemetry(this IServiceCollection services, string[] activityModuleNames)
+    {
+        services
+            .AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService("ItemsShop"))
+            .WithTracing(tracing =>
+            {
+                tracing
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddNpgsql()
+                    .AddSource(activityModuleNames);
+
+                tracing
+                    .AddOtlpExporter();
+            });
+
         return services;
     }
 }
